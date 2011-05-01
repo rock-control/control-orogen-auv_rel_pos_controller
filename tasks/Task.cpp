1da3710360e7b0620a72bd1957581fc16029b12d
Sascha Arnold <sascha.arnold@dfki.de>
2011-05-01 21:50:50 +0200

    first version

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -7,44 +7,199 @@ using namespace auv_rel_pos_controller;
 Task::Task(std::string const& name, TaskCore::TaskState initial_state)
     : TaskBase(name, initial_state)
 {
+    
 }
 
 Task::~Task()
 {
 }
 
+void Task::constrainAngle(double& angle)
+{
+    if (angle < -M_PI)
+        angle = angle + 2 * M_PI;
+    else if (angle > M_PI)
+        angle = angle - 2 * M_PI;
+}
 
+void Task::constrainValue(double& value, const double& contraint)
+{
+    if (value > contraint)
+        value = contraint;
+    else if (value < -contraint)
+        value = -contraint;
+}
+
+void Task::constrainValues(base::AUVPositionCommand& posCommand)
+{
+    constrainValue(posCommand.x, xyConstraint);
+    constrainValue(posCommand.y, xyConstraint);
+    
+    if (_rel_z.get()) 
+    {
+        constrainValue(posCommand.z, zConstraint);
+    }
+    else 
+    {
+        if (posCommand.z > 0)
+            posCommand.z = 0;
+        if (_fixed_z.get() > 0)
+            _fixed_z.set(0);
+    }
+    
+    if(_rel_heading.get())
+    {
+        constrainValue(posCommand.heading, angleConstraint);
+    }
+    else
+    {
+        constrainAngle(posCommand.heading);
+    }
+}
 
 /// The following lines are template definitions for the various state machine
 // hooks defined by Orocos::RTT. See Task.hpp for more detailed
 // documentation about them.
 
-// bool Task::configureHook()
-// {
-//     if (! TaskBase::configureHook())
-//         return false;
-//     return true;
-// }
-// bool Task::startHook()
-// {
-//     if (! TaskBase::startHook())
-//         return false;
-//     return true;
-// }
-// void Task::updateHook()
-// {
-//     TaskBase::updateHook();
-// }
-// void Task::errorHook()
-// {
-//     TaskBase::errorHook();
-// }
-// void Task::stopHook()
-// {
-//     TaskBase::stopHook();
-// }
-// void Task::cleanupHook()
-// {
-//     TaskBase::cleanupHook();
-// }
+bool Task::configureHook()
+{
+    xPID = new motor_controller::PID();
+    yPID = new motor_controller::PID();
+    
+    xPID->setPIDSettings(_controller_x.get());
+    yPID->setPIDSettings(_controller_y.get());
+    
+    //tests:
+    std::cout << _controller_x.get().YMax <<std::endl;
+    base::AUVPositionCommand posCommand;
+    posCommand.heading = 2*M_PI;
+    posCommand.x = 12;
+    posCommand.y = -8;
+    posCommand.z = 2;
+    constrainValues(posCommand);
+    std::cout << posCommand.x << ", " << posCommand.y << ", " << posCommand.z << ", " << posCommand.heading << std::endl;
+    //
+    
+    bodyState.invalidate();
+    taskPeriod = 0;
+    timeout = 0;
+    validBodyState = false;
+    
+    return true;
+}
+bool Task::startHook()
+{
+    // check if input ports are connected
+    if (!_position_command.connected())
+    {
+        std::cerr << TaskContext::getName() << ": " 
+                    << "Input port 'position_command' is not connected." << std::endl;
+        return false;
+    }
+    else if (!_position_sample.connected())
+    {
+        std::cerr << TaskContext::getName() << ": "
+                    << "Input port 'position_sample' is not connected." << std::endl;
+        return false;
+    }
+    
+    return true;
+}
+void Task::updateHook()
+{
+    // get triggering interval of this task context
+    if (taskPeriod <= 0) 
+    {
+        taskPeriod = TaskContext::getPeriod();
+        if (taskPeriod <= 0)
+        {
+            std::cerr << TaskContext::getName() << ": " 
+                        << "This task needs to be a periodic triggering task." << std::endl;
+            return;
+        }
+    }
+    
+    if(_position_command.readNewest(positionCommand) != RTT::NoData) 
+    {
+        timeout = 0;
+        constrainValues(positionCommand);
+    }
+    else 
+    {
+        timeout += taskPeriod;
+        if (timeout >= _timeout.get()) 
+        {
+            std::cerr << TaskContext::getName() << ": "
+                        << "Receiving no new AUVPositionCommand data for longer than "
+                            << _timeout.get() << " seconds." << std::endl;
+            std::cerr << TaskContext::getName() << ": "
+                        << "Switching to task state RUNTIME_ERROR now!" << std::endl;
+            error();
+            return;
+        }
+    }
+    
+    if(_position_sample.readNewest(bodyState) != RTT::NoData)
+    {
+        validBodyState = true;
+    }
+    else
+    {
+        if(!validBodyState && (_rel_z.get() || _rel_heading.get()))
+        {
+            std::cerr << TaskContext::getName() << ": " 
+                        << "Waiting for a valid RigidBodyState to set relative values"
+                            << " for z or heading." << std::endl;
+            return;
+        }
+    }
+    
+    base::AUVMotionCommand motion_command;
+    // set x,y speed
+    motion_command.x_speed = xPID->update(0, positionCommand.x);
+    motion_command.y_speed = yPID->update(0, positionCommand.y);
+    
+    // set depth
+    if (_fixed_z.get() > -9999)
+    {
+        motion_command.z = _fixed_z.get();
+    }
+    else if (_rel_z.get())
+    {
+        motion_command.z = bodyState.position.z() + positionCommand.z;
+        if (motion_command.z > 0)
+            motion_command.z = 0;
+    }
+    else
+    {
+        motion_command.z = positionCommand.z;
+    }
+    
+    // set heading
+    if (_rel_heading.get())
+    {
+    }
+    else
+    {
+        motion_command.heading = positionCommand.heading;
+    }
+    
+    // write motion command
+    if(_motion_command.connected())
+        _motion_command.write(motion_command);
+    else
+        std::cerr << TaskContext::getName() << ": " 
+                    << "Output port motion_command doesn't seem to be connected." << std::endl;
+}
+void Task::errorHook()
+{
+}
+void Task::stopHook()
+{
+}
+void Task::cleanupHook()
+{
+    delete xPID;
+    delete yPID;
+}
 
--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -4,14 +4,29 @@
 #define AUV_REL_POS_CONTROLLER_TASK_TASK_HPP
 
 #include "auv_rel_pos_controller/TaskBase.hpp"
+#include <PID.hpp>
 
 namespace auv_rel_pos_controller {
+    
+    const static double xyConstraint = 10;
+    const static double zConstraint = 3;
+    const static double angleConstraint = M_PI;
+    
     class Task : public TaskBase
     {
 	friend class TaskBase;
     protected:
-
-
+        void constrainValues(base::AUVPositionCommand& posCommand);
+        void constrainValue(double& value, const double& contraint);
+        void constrainAngle(double& angle);
+        
+        motor_controller::PID* xPID;
+        motor_controller::PID* yPID;
+        base::AUVPositionCommand positionCommand;
+        base::samples::RigidBodyState bodyState;
+        double taskPeriod;
+        double timeout;
+        bool validBodyState;
 
     public:
         Task(std::string const& name = "auv_rel_pos_controller::Task", TaskCore::TaskState initial_state = Stopped);
@@ -31,14 +46,14 @@ namespace auv_rel_pos_controller {
          *     ...
          *   end
          */
-        // bool configureHook();
+        bool configureHook();
 
         /** This hook is called by Orocos when the state machine transitions
          * from Stopped to Running. If it returns false, then the component will
          * stay in Stopped. Otherwise, it goes into Running and updateHook()
          * will be called.
          */
-        // bool startHook();
+        bool startHook();
 
         /** This hook is called by Orocos when the component is in the Running
          * state, at each activity step. Here, the activity gives the "ticks"
@@ -54,7 +69,7 @@ namespace auv_rel_pos_controller {
          * component is stopped and recover() needs to be called before starting
          * it again. Finally, FatalError cannot be recovered.
          */
-        // void updateHook();
+        void updateHook();
 
         /** This hook is called by Orocos when the component is in the
          * RunTimeError state, at each activity step. See the discussion in
@@ -62,18 +77,18 @@ namespace auv_rel_pos_controller {
          *
          * Call recovered() to go back in the Runtime state.
          */
-        // void errorHook();
+        void errorHook();
 
         /** This hook is called by Orocos when the state machine transitions
          * from Running to Stopped after stop() has been called.
          */
-        // void stopHook();
+        void stopHook();
 
         /** This hook is called by Orocos when the state machine transitions
          * from Stopped to PreOperational, requiring the call to configureHook()
          * before calling start() again.
          */
-        // void cleanupHook();
+        void cleanupHook();
     };
 }
 
